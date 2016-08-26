4041895a0321bdd5621686009fd25929ee9cd21e
Evgeniy Lesovykh <elesovykh@andiks.com>
2016-08-26 15:15:07 +0300

    добавил пробную либу для grid

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,16 @@
-import React, {Component} from "react";
+﻿import React, {Component} from "react";
 import {Link,Router} from "react-router";
 import AppBar from 'material-ui/AppBar';
 import {Tabs, Tab} from 'material-ui/Tabs';
-
+import Title from '../component/Title';
 
 /*TODO old title, remove*/
 const App = React.createClass({
     render: function () {
         return (
           <div className="layout">
-            <AppBar               
-              title="vpiski2"           
-              iconElementRight={
-                  <div>
-                    <Link to="/map"> map demo </Link>
-                    <Link to="/list"> list demo </Link>
-                    <Link to="/event">Event</Link>
-                  </div>            
-                }         
-            />   
-          
+	    <Title/>
+	
             <Tabs ref="tabPanel" onChange={this.onTap}>
               <Tab label="Поиск"></Tab>
               <Tab label="Знакомства"></Tab>
