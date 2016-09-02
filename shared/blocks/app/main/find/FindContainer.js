2353b636f85000e480f50c8fd1ca4241a4d975ae
4dinterface <4dinterface@mail.ru>
2016-09-02 17:05:13 +0400

    восстановленный blocks

---
+++ shared/blocks/app/main/find/FindContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+//пример использования картинки
+//<img src={require("./img/target.png")} />
+
+export default class FindContainer extends Component {
+  render() {
+    return (
+      <div>123</div>
+    )
+  }
+}
---
+++ shared/blocks/app/meeting-view/MeetingView.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import Avatar from 'material-ui/Avatar';
+import TextField from 'material-ui/TextField';
+import TitleBack from '../title-back/TitleBack';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 50,
+  }
+};
+
+
+class MeetingView extends Component {
+  render() {
+    return (
+      <div style={{display:"flex", flexDirection:"column"}}>
+        <TitleBack title="New meeting"/>
+        <div style={styles.container}>
+          <Avatar src="images/uxceo-128.jpg" size={150}/>
+          <h4>Супер тусовка!</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MeetingView;
